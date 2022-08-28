130df2821814ed328e8f665ab27cc9a32ebb98f0
tarball <kaderarnold@i8cloud.com>
2022-08-28 16:02:58 -0400

    sinewave

--- doc/md/stripe.md
+++ doc/md/stripe.md
@@ -1,14 +1,19 @@
 # stripe definition
 ```lua
   stripe = {}
-  stripe["n"] = 2 -- dimensions, aka d
-  stripe["black"]  = {0,0,0,0} 
-  stripe["white"]  = {1,1,1,1}
-  stripe["color"]  = stripe.black
-  stripe["width"]  = love.graphics.getWidth()/stripe.n
-  stripe["height"] = love.graphics.getHeight()
-  stripe["x"]      = 0
-  stripe["y"]       = stripe.height
+
+  stripe["n"]           = 2 -- dimensions, aka d
+  stripe["black"]       = {0,0,0,0} 
+  stripe["white"]       = {1,1,1,1}
+	stripe["alternative"] = {0.5, 0.5, 0.5, .5}
+  stripe["color"]       = stripe.black
+
+  stripe["width"]       = love.graphics.getWidth()/stripe.n
+  stripe["height"]      = love.graphics.getHeight()
+
+  stripe["x"]           = 0
+  stripe["y"]           = stripe.height
+
 ```
 
 ```lua
--- src/main.lua
+++ src/main.lua
@@ -10,24 +10,34 @@
 
 
 function love.load()
-  love.graphics.setBackgroundColor(stripe.white)
+  --love.graphics.setBackgroundColor(stripe.white)
+print "loading..."
 end
 
 
 
 function love.update()
-	if flip == 1 then
-	  flip = 0
+	if flip == 0 then
 		love.graphics.setBackgroundColor(stripe.white)
+		--stripe.y = stripe.y + 1
+	elseif flip == 1 then
+		love.graphics.setBackgroundColor(stripe.alternative)
 	else
-		flip = 1
 		love.graphics.setBackgroundColor(stripe.black)
+		flip = -1
 	end
+
+  stripe.n = stripe.n + 1
+
+	flip = flip + 1
+	--print ("hi")
+  os.execute("sleep 1")
+
 end
 
 
 
 function love.draw()
-	love.graphics.rectangle("line", stripe.width, stripe.height, stripe.x, stripe.y)
+	love.graphics.rectangle("fill", stripe.width, stripe.height, stripe.x, stripe.y)
 end
 
